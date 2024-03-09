4e930247d7d337197dd708800458ec71d12aae6e
Vitor Rafael <rqfvitor@gmail.com>
2024-03-09 11:36:23 -0300

    add logging to publisher

--- mqtt_publisher/publisher.py
+++ mqtt_publisher/publisher.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 from dotenv import dotenv_values
+from datetime import datetime
 
 config = dotenv_values(".env")
 
@@ -7,5 +8,5 @@ mqtt_client = mqtt.Client(config["PUBLISHER_NAME"])
 mqtt_client.connect(host=config["HOST"], port=int(config["PORT"]))
 
 def publish(text: str):
-    print(f"Publishing {text}")
+    print(f"[{datetime.now().strftime('%Y-%m-%d - %H:%M:%S')}] publishing to {config['RES_TOPIC']}")
     mqtt_client.publish(topic=config["RES_TOPIC"], payload=text)
